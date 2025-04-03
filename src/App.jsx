01dbdd48880ed08b3be90f4859a633ae20d459c8
Hana Ayalew <163385220+Hanah29@users.noreply.github.com>
2025-04-02 23:33:54 -0700

    finish p

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import "./App.css";
 import TaskForm from './components/TaskForm';
 import TaskColumn from './components/TaskColumn';
@@ -6,15 +6,48 @@ import  todoIcon from  './assets/doo.png'
 import doingIcon from  './assets/support.png'
 import doneIcon from  './assets/coffee-cup.png'
 
+
+
+const oldTasks = localStorage.getItem("tasks");
+console.log(oldTasks);
+
 const App = () => {
+  const [tasks, setTasks] = useState(JSON.parse(oldTasks) || []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+  }, [tasks])
+
+  const handleDelete = (taskIndex) => {
+     const newTasks =tasks.filter((task, index) => index !== taskIndex)
+     setTasks(newTasks)
+  }
   return (
     <div className='app'>
-      <TaskForm/>
+      <TaskForm setTasks={setTasks}/>
       <main className='app_main'>
-        <TaskColumn title="To do" icon={todoIcon}/>
-        <TaskColumn title="Doing"  icon={doingIcon}/>
-        <TaskColumn title="Done"  icon={doneIcon}/>
-        {/* <section className='task_column'>section 2</section> */}
+        <TaskColumn 
+             title="To do"
+         icon={todoIcon} 
+         tasks={tasks} 
+         status="todo"
+         handleDelete={handleDelete}
+         />
+         
+        <TaskColumn 
+           title="Doing" 
+         icon={doingIcon} 
+         tasks={tasks} 
+         status="doing"
+         handleDelete={handleDelete}
+         />
+        <TaskColumn 
+            title="Done" 
+            icon={doneIcon}
+             tasks={tasks} 
+            status="done"
+            handleDelete={handleDelete}
+            />
       </main>
     </div>
   )
